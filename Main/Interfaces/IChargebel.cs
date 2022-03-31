244518d47f07b84494042113647299c719972b22
Artem Yakhno <6918735@stud.nau.edu.ua>
2022-03-31 17:38:05 +0300

    Added Interfaces

---
+++ Main/Interfaces/IChargebel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Interfaces
+{
+    public interface IChargebel
+    {
+        void usingEnergy(int time, int requiredСharge);
+        void countingEnergiesOverTime(int time, int requiredСharge);
+        void calculatePercentageOfCharge();
+        void calculateChargeTimeLeft();
+        void transformateChargeTimeLeft();
+    };
+    public interface IWatchebel
+    {
+        string watchVideo(int time);
+    };
+    public interface IPlayebel
+    {
+        string playVideoGames(int time);
+    };
+    public interface IListenebel
+    {
+        string listenMusic(int time);
+    };
+    public interface IWorkingbel
+    {
+        string work(int time);
+    };
+}
